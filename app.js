ccf4520ce785b1da58c5eb25dca4f981c0c28aab
atmabdullaham <atmabdullaham@gmail.com>
2024-02-27 17:46:59 +0600

    add function to phone increases and decrease button

---
+++ app.js
@@ -0,0 +1,27 @@
+function updateProductNumber(product, price, isIncreasing) {
+ const productInput = document.getElementById(product + '-number');
+ let productNumber = productInput.value;
+ if (isIncreasing === true) {
+  productNumber = parseInt(productNumber) + 1;
+ } else if (productNumber > 0) {
+  productNumber = parseInt(productNumber) - 1;
+ }
+ productInput.value = productNumber;
+ //update product total 
+ const productTotal = document.getElementById(product + '-total');
+ productTotal.innerText = productNumber * price;
+
+ // calculateTotal()
+calculateTotal();
+
+}
+
+//handle phone increase and decrease
+// 1. phone increase
+document.getElementById('phone-plus').addEventListener('click', function () {
+ updateProductNumber('phone', 1219, true)
+})
+//2. phone decrease
+document.getElementById('phone-minus').addEventListener('click', function () {
+ updateProductNumber('phone', 1219, false)
+})
